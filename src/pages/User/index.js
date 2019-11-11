56d40c47eacc2bfd4dd66e20c0b18e98a0b3b9bf
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-11 20:15:57 -0300

    fetch data in api from user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User({ navigation }) {
+export default class User extends Component {
 
-  console.tron.log(navigation.getParam('user'))
-  return <View />;
+  static navigationOptions = {
+    title: 'Usuários'
+  }
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  }
+  state = {
+    stars: [],
+  }
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+    this.setState({ stars: response.data });
+
+  }
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
 }
