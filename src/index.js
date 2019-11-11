1f2230b0bb88083361546d0bb89dd179616ce7db
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-11 18:01:21 -0300

    config routes from app

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  StyleSheet,
-  View
-} from 'react-native';
 import './config/reactotronConfig';
+import Routes from './routes';
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello World</Text>
-
-      </View>
+      <Routes />
     );
   }
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 40
-  }
-})
 export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class Main extends Component {
+  render() {
+    return <View />;
+  }
+}
+Main.navigationOptions = {
+  title: 'Hello World'
+}
